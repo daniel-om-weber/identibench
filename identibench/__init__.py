5da5b8ec99071d9343f125b843ea052dfa6dedbb
Daniel Weber <daniel.om.weber@gmail.com>
2025-05-01 11:41:38 +0200

    added run_multiple_benchmarks added __init__.py imports

--- identibench/__init__.py
+++ identibench/__init__.py
@@ -1,30 +1,54 @@
 __version__ = "0.0.8"
 
-from identibench.datasets.workshop import *
-from identibench.datasets.industrial_robot import *
-from identibench.datasets.ship import *
-from identibench.datasets.quad_pelican import *
-from identibench.datasets.quad_pi import *
-from identibench.datasets.broad import *
-from pathlib import Path
+from .benchmark import run_benchmark, BenchmarkSpecSimulation, BenchmarkSpecPrediction, TrainingContext
+from . import metrics
+from . import datasets
 
+# Workshop Benchmarks
+from .datasets.workshop import (
+    BenchmarkWH_Simulation, BenchmarkWH_Prediction,
+    BenchmarkSilverbox_Simulation, BenchmarkSilverbox_Prediction,
+    BenchmarkCascadedTanks_Simulation, BenchmarkCascadedTanks_Prediction,
+    BenchmarkEMPS_Simulation, BenchmarkEMPS_Prediction,
+    BenchmarkNoisyWH_Simulation, BenchmarkNoisyWH_Prediction
+)
+# Robot Benchmarks
+from .datasets.industrial_robot import (
+    BenchmarkRobotForward_Simulation, BenchmarkRobotForward_Prediction,
+    BenchmarkRobotInverse_Simulation, BenchmarkRobotInverse_Prediction
+)
+# Ship Benchmark
+from .datasets.ship import BenchmarkShip_Simulation, BenchmarkShip_Prediction
+# Quadrotor Benchmarks
+from .datasets.quad_pelican import BenchmarkQuadPelican_Simulation, BenchmarkQuadPelican_Prediction
+from .datasets.quad_pi import BenchmarkQuadPi_Simulation, BenchmarkQuadPi_Prediction
+# Broad Benchmark
+# from .datasets.broad import BenchmarkBroad_Simulation, BenchmarkBroad_Prediction
 
-all_dataset_loader = [
-    wiener_hammerstein,
-    silverbox,
-    cascaded_tanks,
-    emps,
-    noisy_wh,
-    robot_forward,
-    robot_inverse,
-    ship,
-    quad_pelican,
-    quad_pi,
-    broad
-]
+simulation_benchmarks = {
+    'WH_Sim': BenchmarkWH_Simulation,
+    'Silverbox_Sim': BenchmarkSilverbox_Simulation,
+    'Tanks_Sim': BenchmarkCascadedTanks_Simulation,
+    'EMPS_Sim': BenchmarkEMPS_Simulation,
+    'NoisyWH_Sim': BenchmarkNoisyWH_Simulation,
+    'RobotForward_Sim': BenchmarkRobotForward_Simulation,
+    'RobotInverse_Sim': BenchmarkRobotInverse_Simulation,
+    'Ship_Sim': BenchmarkShip_Simulation,
+    'QuadPelican_Sim': BenchmarkQuadPelican_Simulation,
+    'QuadPi_Sim': BenchmarkQuadPi_Simulation,
+}
 
-def download_all_datasets(save_path):
-    'Download all datasets provided by identibench in subdirectories'
-    save_path = Path(save_path)
-    for loader in all_dataset_loader:
-        loader(save_path / loader.__name__)
+prediction_benchmarks = {
+    'WH_Pred': BenchmarkWH_Prediction,
+    'Silverbox_Pred': BenchmarkSilverbox_Prediction,
+    'Tanks_Pred': BenchmarkCascadedTanks_Prediction,
+    'EMPS_Pred': BenchmarkEMPS_Prediction,
+    'NoisyWH_Pred': BenchmarkNoisyWH_Prediction,
+    'RobotForward_Pred': BenchmarkRobotForward_Prediction,
+    'RobotInverse_Pred': BenchmarkRobotInverse_Prediction,
+    'Ship_Pred': BenchmarkShip_Prediction,
+    'QuadPelican_Pred': BenchmarkQuadPelican_Prediction,
+    'QuadPi_Pred': BenchmarkQuadPi_Prediction,
+}
+
+all_benchmarks = {**simulation_benchmarks, **prediction_benchmarks}
